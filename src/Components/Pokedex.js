7cba0fac76d7a5e5d21490799c751183d19e510c
Matias Ovejero <61472081+MatiasOvejero@users.noreply.github.com>
2021-01-20 15:53:36 -0300

    mejora visual y comments para mejorar lo hecho

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -12,8 +12,8 @@ const Pokedex = ({ pokemon, type, stats, id, moves, img }) => {
           <img src={img} alt={`Imagen de ${pokemon}`} />
         </div>
       </div>
-      <div id="rightSide">
-        <div id="data">
+      <div id="rightSide" className="pokedex__rightside">
+        <div className="rightside__data">
           {type && <p>Clase: {type}</p>}
           <ul>
             {stats &&
@@ -29,6 +29,30 @@ const Pokedex = ({ pokemon, type, stats, id, moves, img }) => {
                 .filter((move, index) => index < 4)
                 .map((move, index) => <li key={index}> {move.move.name}</li>)}
           </ul>
+        </div>
+        <div className="rightside__paint">
+          <div className="paint__blue">
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+            <div className="paint">1</div>
+          </div>
+          <div className="paint__orange">
+            {/* cuando aprete front button muestra la imagen comun */}
+            <button id="frontButton" className="orange__button">
+              {" "}
+              Comun{" "}
+            </button>
+            {/* cuando aprete shiny button muestra un shiny  */}
+            <button id="shinyButton" className="orange__button">
+              {" "}
+              Shiny
+            </button>
+          </div>
         </div>
       </div>
     </div>
