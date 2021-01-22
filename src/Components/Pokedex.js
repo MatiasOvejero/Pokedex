05f7497005f278862615daf846cd7ca781fa5c2a
Matias Ovejero <61472081+MatiasOvejero@users.noreply.github.com>
2021-01-22 13:17:04 -0300

    se paso todo a Sass, mejora en los botones, imagen, y se arreglo problema de valores en los tamaño

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import Print from "./Print";
-const Pokedex = ({ pokemon, type, stats, id, moves, img, shiny, female }) => {
-  const [mods, setmods] = useState(" ");
+import Search from "./Search";
+const Pokedex = ({
+  pokemon,
+  type,
+  stats,
+  id,
+  moves,
+  img,
+  shiny,
+  female,
+  search,
+}) => {
+  const [mods, setmods] = useState();
   console.log(mods);
   return (
     <div id="Pokedex" className="pokedex">
-      <div id="leftSide" className="pokedex__leftside">
-        <div id="nameId" className="pokedex__nameid">
+      <div className="pokedex__leftside">
+        <div className="pokedex__nameid">
           <p>{pokemon}</p>
           {id && <p>#{id}</p>}
         </div>
@@ -18,63 +29,44 @@ const Pokedex = ({ pokemon, type, stats, id, moves, img, shiny, female }) => {
           shiny={shiny}
         />
       </div>
-      <div id="rightSide" className="pokedex__rightside">
-        <div className="rightside__data">
+      <div className="pokedex__rightside">
+        <Search getPok={search} />
+        <div className="pokedex__data">
           {type && <p>Clase: {type}</p>}
-          <ul>
+          <ul className="pokedex__data__stats">
             {stats &&
               stats.map((item, index) => (
                 <li key={index}>
-                  {item.stat.name}: {item.base_stat}
+                  <b>{item.stat.name}</b>: {item.base_stat}
                 </li>
               ))}
           </ul>
-          <ul>
+          <p>Habilidades: </p>
+          <ul className="pokedex__data__moves">
             {moves &&
               moves
                 .filter((move, index) => index < 4)
                 .map((move, index) => <li key={index}> {move.move.name}</li>)}
           </ul>
         </div>
-        <div className="rightside__paint">
-          <div className="paint__blue">
-            <div className="paint"></div>
-            <div className="paint"></div>
-            <div className="paint"></div>
-            <div className="paint"></div>
-            <div className="paint"></div>
-            <div className="paint"></div>
-            <div className="paint"></div>
-            <div className="paint"></div>
+        <div className="pokedex__buttons">
+          <div className="pokedex__buttons__blue">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
-          <div className="paint__orange">
+          <div className="pokedex__buttons__orange">
             {/* cuando aprete front button muestra la imagen comun */}
-            <button
-              onClick={() => setmods("D")}
-              id="frontButton"
-              className="orange__button"
-            >
-              {" "}
-              Comun{" "}
-            </button>
+            <button onClick={() => setmods()}>Comun</button>
             {/* apretar este boton muestra a la pokemona */}
-            <button
-              onClick={() => setmods("F")}
-              id="femaleButton"
-              className="orange__button"
-            >
-              {" "}
-              Pokemona{" "}
-            </button>
+            <button onClick={() => setmods("F")}>Pokemona</button>
             {/* cuando aprete shiny button muestra un shiny  */}
-            <button
-              onClick={() => setmods("S")}
-              id="shinyButton"
-              className="orange__button"
-            >
-              {" "}
-              Shiny
-            </button>
+            <button onClick={() => setmods("S")}>Shiny</button>
           </div>
         </div>
       </div>
--- src/Components/Print.js
+++ src/Components/Print.js
@@ -2,15 +2,39 @@ import React from "react";
 
 const Print = (props) => {
   return (
-    <div id="img">
-      {props.change == "S" && (
-        <img src={props.shiny} alt={`Imagen shiny de ${props.pokemon}`} />
-      )}
-      {props.change == "F" && (
-        <img src={props.female} alt={`Imagen femenina de ${props.pokemon}`} />
-      )}
-      {props.change == "D" && (
-        <img src={props.img} alt={`Imagen de ${props.pokemon}`} />
+    <div className="print">
+      {props.change == "S" &&
+        (props.shiny ? (
+          <img
+            src={props.shiny}
+            alt={`Imagen shiny de ${props.pokemon}`}
+            className="print__image"
+          />
+        ) : (
+          <p>No hay shiny</p>
+        ))}
+      {props.change == "F" &&
+        (props.female ? (
+          <img
+            src={props.female}
+            alt={`Imagen femenina de ${props.pokemon}`}
+            className="print__image"
+          />
+        ) : (
+          <div>
+            <img
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/48a3c21c-75ee-4b46-81de-b48ebb9e754f/da9u608-3f0b40a9-103b-4198-86b0-9f8618e2fe44.png/v1/fill/w_927,h_862,q_75,strp/mimikyu_by_toalugia-da9u608.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi80OGEzYzIxYy03NWVlLTRiNDYtODFkZS1iNDhlYmI5ZTc1NGYvZGE5dTYwOC0zZjBiNDBhOS0xMDNiLTQxOTgtODZiMC05Zjg2MThlMmZlNDQucG5nIiwid2lkdGgiOiI8PTkyNyIsImhlaWdodCI6Ijw9ODYyIn1dXX0.s3rVzdSvUbSiSB5dVOse4i8qIst0uV6WDEx9LWJUkRg"
+              className="print__image"
+            />
+            <p className="print__error">No hay female</p>
+          </div>
+        ))}
+      {!props.change && (
+        <img
+          src={props.img}
+          alt={`Imagen de ${props.pokemon}`}
+          className="print__image"
+        />
       )}
       {/* <img src={props.img} alt={`Imagen de ${props.pokemon}`} /> */}
     </div>
