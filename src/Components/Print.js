a57bf19e84532eaae1df6b57c637d3c22f49e8f6
MatiasOvejero <matilmds91@gmail.com>
2021-01-25 10:09:41 -0300

    fetch

--- src/Components/Print.js
+++ src/Components/Print.js
@@ -3,13 +3,13 @@ import React from "react";
 const Print = (props) => {
   return (
     <div id="img">
-      {props.change == "S" && (
+      {props.change === "S" && (
         <img src={props.shiny} alt={`Imagen shiny de ${props.pokemon}`} />
       )}
-      {props.change == "F" && (
+      {props.change === "F" && (
         <img src={props.female} alt={`Imagen femenina de ${props.pokemon}`} />
       )}
-      {props.change == "D" && (
+      {props.change === "D" && (
         <img src={props.img} alt={`Imagen de ${props.pokemon}`} />
       )}
       {/* <img src={props.img} alt={`Imagen de ${props.pokemon}`} /> */}
